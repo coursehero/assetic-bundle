466ccb8d3adfbe9069a843ea947bb567d31e58b8
Connor Clark <connor.clark@coursehero.com>
2018-05-29 13:17:46 -0700

    dont do custom asset collection when debug is on

--- src/Assetic/CHAssetFactory.php
+++ src/Assetic/CHAssetFactory.php
@@ -3,11 +3,17 @@
 namespace CourseHero\UtilsBundle\Assetic;
 
 use Symfony\Bundle\AsseticBundle\Factory\AssetFactory;
+use Assetic\Asset\AssetCollection;
 
 class CHAssetFactory extends AssetFactory
 {
     protected function createAssetCollection(array $assets = [], array $options = [])
     {
+        if ($this->isDebug()) {
+            // fall back to default behavior when in debug mode
+            return new AssetCollection($assets, [], null, isset($options['vars']) ? $options['vars'] : []);
+        }
+
         return new CHAssetCollection($assets, [], null, isset($options['vars']) ? $options['vars'] : []);
     }
 }
